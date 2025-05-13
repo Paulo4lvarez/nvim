3cda5f28813a86d47dc42dd4cbf44a84ee8db986
Paul Alvarez <palvarez-2023238@kinal.edu.gt>
2025-05-13 07:50:49 -0600

    testing: modified way of accessing OS icon and added a nicer fallback

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -118,7 +118,7 @@ return {
           Ubuntu = "󰕈",
           Linux = "󰌽",
           FreeBSD = " ",
-          Darwin = "",
+          OSX = "",
           Windows = ""
         }
         local sysname = jit.os
@@ -126,7 +126,7 @@ return {
           local distro = vim.fn.system('lsb_release -si'):gsub('\n', '')
           _G.os_icon = os_icons[distro] or "󰌽" -- Fallback Linux icon
         else
-          _G.os_icon = os_icons[sysname] or "󰌽"
+          _G.os_icon = os_icons[sysname] or ""
         end
         return _G.os_icon
       end,
