a890b6e63808dd22ede963298b90649403e8c65e
Paulo Alvarez <palvarez-2023238@kinal.edu.gt>
2025-05-25 00:28:09 -0600

    modified: snacks dashboard config.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -17,18 +17,18 @@ fast as fuck, boi
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "g", desc = "Git", action = "<CMD>Neogit<CR>" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
 
-      -- Use the compact files layout structure
       sections = {
         { section = "header" },
         { icon = "", section = "keys", gap = 1, padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", padding = 1 },
+        { icon = " ", title = "Projects", padding = 1 },
+        { section = "projects", padding = 1 },
         { section = "startup" },
       },
 
@@ -36,5 +36,6 @@ fast as fuck, boi
         winhighlight = "Normal:Normal,NormalFloat:Normal",
       },
     },
+    indent = { only_current = true, animate = { enabled = false } },
   },
 }
